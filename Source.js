1d878028d8af391718ecefa62862de7ac81d7978
Zinal001 <LaNiZ.001@gmail.com>
2015-12-15 17:14:15 +0100

    Changes according to Changelog 2015-12-09
    Due to the large amount of changes in the 12/09-15 update, I've decided
    to rewrite the whole documentation.
    
    I've also deleted all classes that are not part of the in-game code,
    such as Memory, Owner, PathStep, SurvivalInfo and global.

--- Source.js
+++ Source.js
@@ -4,50 +4,49 @@
  * @class
  * @constructor
  */
-Source = function () {
+Source = function() { };
 
-};
+Source.prototype =
+{
+    /**
+     * The remaining amount of energy.
+     *
+     * @type {number}
+     */
+    energy: 0,
 
-Source.prototype = {
-	/**
-	 * A unique object identificator.
-	 *
-	 * @type string
-	 */
-	id: "",
+    /**
+     * The total amount of energy in the source. Equals to 3000 in most cases.
+     *
+     * @type {number}
+     */
+    energyCapacity: 0,
 
-	/**
-	 * The link to the Room object of this structure.
-	 *
-	 * @type Room
-	 */
-	room: null,
+    /**
+     * A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id.
+     *
+     * @type {string}
+     */
+    id: "",
 
-	/**
-	 * An object representing the position of this structure in the room.
-	 *
-	 * @type RoomPosition
-	 */
-	pos: null,
+    /**
+     * An object representing the position of this structure in the room.
+     *
+     * @type {RoomPosition}
+     */
+    pos: null,
 
-	/**
-	 * The remaining amount of energy.
-	 *
-	 * @type number
-	 */
-	energy: 0,
+    /**
+     * The link to the Room object of this structure.
+     *
+     * @type {Room}
+     */
+    room: null,
 
-	/**
-	 * The total amount of energy in the source. Equals to 3000 in most cases.
-	 *
-	 * @type number
-	 */
-	energyCapacity: 0,
-
-	/**
-	 * The remaining time after which the source will be refilled.
-	 *
-	 * @type number
-	 */
-	ticksToRegeneration: 0
+    /**
+     * The remaining time after which the source will be refilled.
+     *
+     * @type {number}
+     */
+    tickToRegeneration: 0
 };
--- Spawn.js
+++ Spawn.js
@@ -4,153 +4,177 @@
  * @class
  * @constructor
  */
-Spawn = function () {
+Spawn = function() { };
 
-};
+Spawn.prototype =
+{
+    /**
+     * The amount of energy containing in the spawn.
+     *
+     * @type {number}
+     */
+    energy: 0,
 
-Spawn.prototype = {
-	/**
-	 * A unique object identificator.
-	 *
-	 * @type {string}
-	 */
-	id: "",
+    /**
+     * The total amount of energy the spawn can contain
+     *
+     * @type {number}
+     */
+    energyCapacity:0,
 
-	/**
-	 * Spawn’s name. You choose the name upon creating a new spawn, and it cannot be changed later. This name is a hash
-	 * key to access the spawn via the Game.spawns object.
-	 *
-	 * @type {string}
-	 */
-	name: "",
+    /**
+     * The current amount of hit points of the spawn.
+     *
+     * @type {number}
+     */
+    hits: 0,
 
-	/**
-	 * An object with the spawn’s owner info containing the following properties:
-	 *
-	 * @type {Owner}
-	 */
-	owner: null,
+    /**
+     * The maximum amount of hit points of the spawn.
+     *
+     * @type {number}
+     */
+    hitsMax: 0,
 
-	/**
-	 * The link to the Room object of this spawn.
-	 *
-	 * @type {Room}
-	 */
-	room: null,
+    /**
+     * A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id.
+     *
+     * @type {string}
+     */
+    id: "",
 
-	/**
-	 * An object representing the position of this spawn in a room.
-	 *
-	 * @type {RoomPosition}
-	 */
-	pos: null,
+    /**
+     * A shorthand to Memory.spawns[spawn.name].
+     * You can use it for quick access the spawn’s specific memory data object.
+     *
+     * @type {object}
+     */
+    memory: null,
 
-	/**
-	 * A shorthand to Memory.spawns[spawn.name]. You can use it for quick access the spawn’s specific memory data object.
-	 *
-	 * @type {any[]}
-	 */
-	memory: [ ],
+    /**
+     * Whether it is your spawn or foe.
+     *
+     * @type {boolean}
+     */
+    my: true,
 
-	/**
-	 * Whether it is your spawn or foe.
-	 *
-	 * @type {boolean}
-	 */
-	my: true,
+    /**
+     * Spawn’s name. You choose the name upon creating a new spawn, and it cannot be changed later.
+     * This name is a hash key to access the spawn via the Game.spawns object.
+     *
+     * @type {string}
+     */
+    name: "",
 
-	/**
-	 * Always equal to ‘spawn’.
-	 *
-	 * @type {string}
-	 */
-	structureType: 'spawn',
+    /**
+     * An object with the spawn’s owner info.
+     *
+     * @type {object}
+     */
+    owner: {
+        /**
+         * The name of the owner user.
+         *
+         * @type {string}
+         */
+        username: ""
+    },
 
-	/**
-	 * If the spawn is in process of spawning a new creep, this object will contain the new creep’s information, or null otherwise.
-	 *
-	 * @return {Creep|null}
-	 */
-	spawning: null,
+    /**
+     * An object representing the position of this spawn in a room.
+     *
+     * @type {RoomPosition}
+     */
+    pos: null,
 
-	/**
-	 * The amount of energy containing in the spawn.
-	 *
-	 * @type {number}
-	 */
-	energy: 0,
+    /**
+     * The link to the Room object of this spawn.
+     *
+     * @type {Room}
+     */
+    room: null,
 
-	/**
-	 * The total amount of energy the spawn can contain
-	 *
-	 * @type {number}
-	 */
-	energyCapacity: 0,
+    /**
+     * Always equal to ‘spawn’.
+     *
+     * @type {string|STRUCTURE_SPAWN}
+     */
+    structureType: STRUCTURE_SPAWN,
 
-	/**
-	 * The current amount of hit points of the spawn.
-	 *
-	 * @type {number}
-	 */
-	hits: 0,
+    /**
+     * If the spawn is in process of spawning a new creep,
+     * this object will contain the new creep’s information, or null otherwise.
+     *
+     * @type {object|null}
+     */
+    spawning: false,
 
-	/**
-	 * The maximum amount of hit points of the spawn.
-	 *
-	 * @type {number}
-	 */
-	hitsMax: 0,
+    /**
+     * Check if a creep can be created.
+     *
+     * @param {string[]} body An array describing the new creep’s body.
+     *                   Should contain 1 to 50 elements with one of these constants: WORK, MOVE, CARRY, ATTACK, RANGED_ATTACK, HEAL, TOUGH
+     * @param {string} [name] The name of a new creep.
+     *                        It should be unique creep name, i.e. the Game.creeps object should not contain another creep with the same name (hash key).
+     *                        If not defined, a random name will be generated.
+     *
+     * @return {number|OK|ERR_NOT_OWNER|ERR_NAME_EXISTS|ERR_BUSY|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_ARGS|ERR_RCL_NOT_ENOUGH}
+     */
+    canCreateCreep: function(body, name) { },
 
-	/**
-	 * Check if a creep can be created.
-	 *
-	 * @param {String[]} body An array describing the new creep’s body. Should contain 1 to 30 elements
-	 * @param {String} [name] The name of a new creep. It should be unique creep name, i.e. the Game.creeps object should not contain another creep with the same name (hash key).
-	 * 						  If not defined, a random name will be generated.
-	 *
-	 * @return {number}
-	 */
-	canCreateCreep: function(body, name) {
-	},
+    /**
+     * Start the creep spawning process.
+     *
+     * @param {string[]} body An array describing the new creep’s body.
+     *                   Should contain 1 to 50 elements with one of these constants: WORK, MOVE, CARRY, ATTACK, RANGED_ATTACK, HEAL, TOUGH
+     * @param {string} [name] The name of a new creep.
+     *                        It should be unique creep name, i.e. the Game.creeps object should not contain another creep with the same name (hash key).
+     *                        If not defined, a random name will be generated.
+     * @param {object} [memory] The memory of a new creep. If provided, it will be immediately stored into Memory.creeps[name].
+     *
+     * @return {string|number|ERR_NOT_OWNER|ERR_NAME_EXISTS|ERR_BUSY|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_ARGS|ERR_RCL_NOT_ENOUGH}
+     */
+    createCreep: function(body, name, memory) { },
 
-	/**
-	 * Start the creep spawning process.
-	 *
-	 * @param {string[]} bodyParts An array describing the new creep’s body. Should contain 1 to 30 elements
-	 * @param {string} [name] The name of a new creep. It should be unique creep name, i.e. the Game.creeps object should not
-	 *  contain another creep with the same name (hash key). If not defined, a random name will be generated.
-	 * @param {any[]} [memory] The memory of a new creep. If provided, it will be immediately stored into Memory.creeps[name].
-	 *
-	 * @return {String, number} The name of a new creep or an error code
-	 */
-	createCreep: function (bodyParts, name, memory) {
-	},
+    /**
+     * Destroy this spawn immediately.
+     *
+     * @return {number|OK|ERR_NOT_OWNER}
+     */
+    destroy: function() { },
 
-	/**
-	 * Destroy this spawn immediately.
-	 * @return {number}
-	 */
-	destroy: function() {
-	},
+    /**
+     * Toggle auto notification when the spawn is under attack.
+     * The notification will be sent to your account email. Turned on by default.
+     *
+     * @param {boolean} enabled Whether to enable notification or disable.
+     *
+     * @return {number|OK|ERR_NOT_OWNER|ERR_INVALID_ARGS}
+     */
+    notifyWhenAttacked: function(enabled) { },
 
-	/**
-	 * Toggle auto notification when the spawn is under attack. The notification will be sent to your account email. Turned on by default.
-	 * @param {boolean} enabled Whether to enable notification or disable.
-	 *
-	 * @return {number}
-	 */
-	notifyWhenAttacked: function(enabled) {
-	},
+    /**
+     * Increase the remaining time to live of the target creep.
+     * The target should be at adjacent square.
+     * The spawn should not be busy with the spawning process.
+     * Each execution increases the creep's timer by amount of ticks according to this formula:
+     *  floor(500/body_size).
+     * Energy required for each execution is determined using this formula:
+     *  ceil(creep_cost/3/body_size).
+     *
+     * @param {Creep} target The target creep object.
+     *
+     * @return {number|OK|ERR_NOT_OWNER|ERR_BUSY|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_TARGET|ERR_FULL|ERR_NOT_IN_RANGE}
+     */
+    renewCreep: function(target) { },
 
-	/**
-	 * Transfer the energy from the spawn to a creep
-	 *
-	 * @param {Creep} target The creep object which energy should be transferred to.
-	 * @param {number} [amount] The amount of energy to be transferred. If omitted, all the remaining amount of energy
-	 *  will be used.
-	 *
-	 *  @return {number}
-	 */
-	transferEnergy: function (target, amount) {
-	}
+    /**
+     * Transfer the energy from the spawn to a creep.
+     *
+     * @param {Creep} target The creep object which energy should be transferred to.
+     * @param {number} [amount] The amount of energy to be transferred. If omitted, all the remaining amount of energy will be used.
+     *
+     * @return {number|OK|ERR_NOT_OWNER|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_TARGET|ERR_FULL|ERR_NOT_IN_RANGE}
+     */
+    transferEnergy: function(target, amount) { }
 };
