b5fffe457dd011035d4acbf7314da13c79654187
Bob Sardinia <shibdib@users.noreply.github.com>
2020-01-09 07:34:04 -0500

    Add isPublic/setPublic

--- Structures/StructureRampart.js
+++ Structures/StructureRampart.js
@@ -4,18 +4,39 @@
  * @class
  * @extends {OwnedStructure}
  *
- * @see {@link http://support.screeps.com/hc/en-us/articles/207712959-StructureRampart}
+ * @see {@link https://docs.screeps.com/api/#StructureRampart.}
  */
 StructureRampart = function() { };
 
 StructureRampart.prototype =
 {
     /**
+     * If false (default), only your creeps can step on the same square. If true, any hostile creeps can pass through.
+     *
+     * @see {@link https://docs.screeps.com/api/#StructureRampart.isPublic}
+     *
+     * @type {boolean}
+     */
+    isPublic: false,
+    /**
      * The amount of game ticks when this rampart will lose some hit points.
      *
-     * @see {@link http://support.screeps.com/hc/en-us/articles/207712959-StructureRampart#ticksToDecay}
+     * @see {@link https://docs.screeps.com/api/#StructureRampart.ticksToDecay}
      *
      * @type {number}
      */
-    ticksToDecay: 0
+    ticksToDecay: 0,
+
+    /**
+     * Make this rampart public to allow other players' creeps to pass through.
+     *
+     * @see {@link https://docs.screeps.com/api/#StructureRampart.setPublic}
+     *
+     * @type {function}
+     *
+     * @param {boolean} Whether this rampart should be public or non-public..
+     *
+     * @return {OK|ERR_NOT_OWNER}
+     */
+    setPublic: function(isPublic) { },
 };
